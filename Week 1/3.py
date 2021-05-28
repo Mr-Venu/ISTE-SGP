9f896e316d55577d8d2ee8375275e8027da20a45
Venumadhav <84865442+Mr-Venu@users.noreply.github.com>
2021-05-28 21:46:23 +0530

    Update 3.py

--- Week 1/3.py
+++ Week 1/3.py
@@ -1,7 +1,8 @@
+#Printing the factorial of a user input number
 n=int(input('Enter a number '))
 t=n
 f=1
 while(n>1):
     f=f*n
     n=n-1
-print('Foactorial of',t,'is',f)
+print('Factorial of',t,'is',f)
